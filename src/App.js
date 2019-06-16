98f5a2b99e5e8468fd8b15e456d10bb0701eb3d0
Dennis Ries <dtries@gmail.com>
2019-06-15 21:34:20 -0600

    changed lists to tables w/out headers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Collapsible, CollapsibleItem, Navbar } from 'react-materialize';
+import { Collapsible, CollapsibleItem, Navbar, Table } from 'react-materialize';
+import TableBody from "./components/TableBody/tablebody";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list1: [{point: "Ties To North Texas"}, {point: "Family"}],
+      list2: [{point: "Collaborative Approach Preferred, Multidimensional As Apropos"}, {point: "Team Building and Staff Development"}],
+      list3: [{point: "Generated All Financial Statements and Provided Analysis for External and Internal Reporting"}, {point: "Forecasted Impact of New Accounting Pronouncements"}, {point: "Prepared Finacial Information for External Surveys"}],
+      list4: [{point: "Managed Payments for Leased Properties for Target Corporation"}, {point: "Acted as Delegated Authority for Contract Approval for State Controller"}, {point: "Verified State Capital Appropriations Complied with State Statutes"}],
+      list5: [{point: "Reported Investment Activity to Board of Trustees"}, {point: "Chaired the Defined Contribution Plan Investment Committee"}, {point: "Allocated Spending Authorization and Monitored Compliance"}],
+      list6: [{point: "Designed and Implemented Administrative Shared Services"}, {point: "Established Change Agent Network"}],
+      list7: [{point: "Managed Budget, Forecast, and Requests"}, {point: "Oversaw Expenditure Compliance & Allowability"}, {point: "Enacted Deficit Account Resolution"}],
+      list8: [{point: "Designed Finacial Operations Websites"}, {point: "Led Function & Testing of Financial and HR Data Systems"}, {point: "Implemented New Travel and Expense Management System"}],
+      list9: [{point: "Developed, Revised, and Enforced University Fiscal Policy and Procedures"}, {point: "Ensured University with Laws and Regulations"}, {point: "Mitigated Risk through Policy and Insurance Review"}],
+      list10: [{point: "Affordable Libral Arts Education"}, {point: "Focus on Building a Strong, Diverse and Vibrant University Community"}, {point: "Collegial Administrative Culture"}]
+    };
+  }
+
   render() {
+
   return (
     <div className="App">
       <Navbar brand={                    
@@ -16,69 +34,104 @@ class App extends Component {
       <div className="container">     
         <Collapsible popout>
             <CollapsibleItem header="Hello" icon="done_outline">
-              <ul>
-                <li>Ties To North Texas</li>
-                <li>Family</li> 
-              </ul>            </CollapsibleItem>
+              <Table className="table_class">
+                {this.state.list1.map((listInfo, index) => 
+                  <TableBody
+                    key = {index}
+                    listItem = {listInfo.point}
+                  />
+                )}
+              </Table>            
+            </CollapsibleItem>
             <CollapsibleItem header="Leadership" icon="done_outline">
-              <ul>
-                <li>Collaborative Approach Preferred, Multidimensional As Apropos </li>
-                <li>Team Building and Staff Development</li> 
-              </ul>
+              <Table className="table_class">
+                {this.state.list2.map((listInfo, index) => 
+                  <TableBody
+                    key = {index}
+                    listItem = {listInfo.point}
+                  />
+                )}
+              </Table>
             </CollapsibleItem>
             <CollapsibleItem header="Institutional Accounting & Financial Reporting" icon="done_outline">
-              <ul>
-                <li>Generated All Financial Statements and Provided Analysis for External and Internal Reporting</li>
-                <li>Forecasted Impact of New Accounting Pronouncements</li> 
-                <li>Prepared Finacial Information for External Surveys</li> 
-              </ul>
+             <Table className="table_class">
+                {this.state.list3.map((listInfo, index) => 
+                  <TableBody
+                    key = {index}
+                    listItem = {listInfo.point}
+                  />
+                )}
+              </Table>
             </CollapsibleItem>
             <CollapsibleItem header="Property, Facilities, & Construction Management" icon="done_outline">
-              <ul>
-                <li>Managed Payments for Leased Properties for Target Corporation</li>
-                <li>Acted as Delegated Authority for Contract Approval for State Controller</li> 
-                <li>Verified State Capital Appropriations Complied with State Statutes </li> 
-              </ul>
+              <Table className="table_class">
+                {this.state.list4.map((listInfo, index) => 
+                  <TableBody
+                    key = {index}
+                    listItem = {listInfo.point}
+                  />
+                )}
+              </Table>
             </CollapsibleItem>
             <CollapsibleItem header="Investment & Endowment Management" icon="done_outline">
-              <ul>
-                <li>Reported Investment Activity to Board of Trustees</li>
-                <li>Chaired the Defined Contribution Plan Investment Committee</li> 
-                <li>Allocated Spending Authorization and Monitored Compliance</li> 
-              </ul>          
+              <Table className="table_class">
+                {this.state.list5.map((listInfo, index) => 
+                  <TableBody
+                    key = {index}
+                    listItem = {listInfo.point}
+                  />
+                )}
+              </Table>         
             </CollapsibleItem>
             <CollapsibleItem header="Administrative Services" icon="done_outline">
-              <ul>
-                <li>Designed and Implemented Administrative Shared Services</li>
-                <li>Established Change Agent Network</li> 
-              </ul>          
+              <Table className="table_class">
+                {this.state.list6.map((listInfo, index) => 
+                  <TableBody
+                    key = {index}
+                    listItem = {listInfo.point}
+                  />
+                )}
+              </Table>          
             </CollapsibleItem>
             <CollapsibleItem header="Budget & Appropriations" icon="done_outline">
-              <ul>
-                <li>Managed Budget, Forecast, and Requests</li>
-                <li>Oversaw Expenditure Compliance & Allowability</li> 
-                <li>Enacted Deficit Account Resolution</li> 
-              </ul>
+              <Table className="table_class">
+                {this.state.list7.map((listInfo, index) => 
+                  <TableBody
+                    key = {index}
+                    listItem = {listInfo.point}
+                  />
+                )}
+              </Table>
             </CollapsibleItem>
             <CollapsibleItem header="Information Systems Administration" icon="done_outline">
-              <ul>
-                <li>Designed Finacial Operations Websites </li>
-                <li>Led Function & Testing of Financial and HR Data Systems</li> 
-                <li>Implemented New Travel and Expense Management System</li> 
-              </ul>          </CollapsibleItem>
+              <Table className="table_class">
+                {this.state.list8.map((listInfo, index) => 
+                  <TableBody
+                    key = {index}
+                    listItem = {listInfo.point}
+                  />
+                )}
+              </Table>         
+            </CollapsibleItem>
             <CollapsibleItem header="Policy, Compliance, & Risk Management" icon="done_outline">
-              <ul>
-                  <li>Developed, Revised, and Enforced University Fiscal Policy and Procedures</li>
-                  <li>Ensured University with Laws and Regulations</li> 
-                  <li>Mitigated Risk through Policy and Insurance Review</li> 
-              </ul> 
+              <Table className="table_class">
+                {this.state.list9.map((listInfo, index) => 
+                  <TableBody
+                    key = {index}
+                    listItem = {listInfo.point}
+                  />
+                )}
+              </Table> 
             </CollapsibleItem>
             <CollapsibleItem header="Why MSU" icon="done_outline">
-              <ul>
-                  <li>Affordable Libral Arts Education</li>
-                  <li>Focus on Building a Strong, Diverse and Vibrant University Community</li> 
-                  <li>Collegial Administrative Culture</li> 
-              </ul>
+              <Table className="table_class">
+                {this.state.list10.map((listInfo, index) => 
+                  <TableBody
+                    key = {index}
+                    listItem = {listInfo.point}
+                  />
+                )}
+              </Table>
             </CollapsibleItem>
         </Collapsible>
       </div>
